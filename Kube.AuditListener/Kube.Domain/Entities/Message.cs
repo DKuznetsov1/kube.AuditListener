8bcde2bd4ab649dabcaae2ac5ecbaa181490998e
Roman Saveliev <rsaveliev@orbus.orbussoftware.com>
2019-08-10 11:46:57 +0300

    add mongo message entity

---
+++ Kube.AuditListener/Kube.Domain/Entities/Message.cs
@@ -0,0 +1,9 @@
+﻿using MongoDB.Bson;
+
+namespace Kube.Domain.Entities
+{
+    public class Message
+    {
+        public ObjectId Name { get; set; }
+    }
+}
--- Kube.AuditListener/Kube.Persistance/AuditListenerContext.cs
+++ Kube.AuditListener/Kube.Persistance/AuditListenerContext.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Driver;
+﻿using Kube.Domain.Entities;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Kube.Persistance
 {
@@ -8,10 +10,15 @@ namespace Kube.Persistance
 
         public AuditListenerContext()
         {
-            var connectionString = "";
+            var connectionString = "mongodb://localhost:27017/kubelistener";
             var connection = new MongoUrlBuilder(connectionString);
             var client = new MongoClient(connectionString);
             database = client.GetDatabase(connection.DatabaseName);
+            var coll = database.GetCollection<Message>("Message");
+
+            var messageId = new ObjectId("5d4e7c720fab82f6735b69af");
+
+            var message = coll.Find(x => x.Name == messageId).Limit(5).ToListAsync();
         }
     }
 }
